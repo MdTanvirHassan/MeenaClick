7fffe9ba8ea6d8afc5c5e99c7f0ae68b709186aa
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-11-17 15:54:05 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import logo2 from '../../image/App-Download-Banner.jpg';
 export default function Header(props) {
   return (
     <div>
-         <header className="p-3 mb-0 border-bottom">
+         <header className="p-3 mb-0 border-bottom ">
     <div className="full-container">
         
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start mx-3">
@@ -15,9 +15,11 @@ export default function Header(props) {
       <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
           <img src={logo} alt='...' className="bi me-2"  width="100" height="60"  aria-label="Bootstrap"/>
         </a>
-        
+        <form className="col-12 col-lg-4 mb-3 mb-lg-0 me-lg-0 text-center mx-5 me-10"  role="search">
+          <input type="search" className="form-control border-success text-end mx-5" placeholder="Search Here..." aria-label="Search"/>
+        </form>
 
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2  mb-md-0 mx-5 d-flex justify-content-end">
             {/* <li><button className='col-sm-0 btn btn-success mt-1 p-2 px-3' type="submit"><i className="bi bi-search"></i></button></li> */}
 
           <li><a href='/' className="nav-link px-2 link-secondary"><button type="button" className={`btn btn-outline-${props.mode==='dark'?'light':'secondary'}`}><i className="bi bi-geo-alt me-1"></i>Area</button></a></li>
@@ -25,38 +27,36 @@ export default function Header(props) {
           <li><a href='/' className="nav-link px-2 link-secondary">
   
 
- <button className={`btn btn-outline-${props.mode==='dark'?'light':'secondary'}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
- <i className="bi bi-person-circle me-1"></i> Log-in/Sign-Up
-</button> </a></li>
+        <button className={`btn btn-outline-${props.mode==='dark'?'light':'secondary'}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <i className="bi bi-person-circle me-1"></i> Log-in/Sign-Up
+        </button> </a></li>
 
-<div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" labelled="offcanvasExampleLabel">
-  <div className="offcanvas-header">
-    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div className="offcanvas-body">
-    <div>
-      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-    </div>
-    <div className="dropdown mt-3">
-      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul className="dropdown-menu">
-        <li><a className="dropdown-item" href='/'>Action</a></li>
-        <li><a className="dropdown-item" href='/'>Another action</a></li>
-        <li><a className="dropdown-item" href='/'>Something else here</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+        <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" labelled="offcanvasExampleLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <div>
+              Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            </div>
+            <div className="dropdown mt-3">
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                Dropdown button
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href='/'>Action</a></li>
+                <li><a className="dropdown-item" href='/'>Another action</a></li>
+                <li><a className="dropdown-item" href='/'>Something else here</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
           <li><a href='/' className="nav-link px-2 link-dark"><button type="button" className={`btn btn-outline-${props.mode==='dark'?'light':'secondary'}`}><i className="bi bi-cart me-1"></i>Cart <span className="badge text-bg-danger rounded-pill align-text-bottom"> 7</span></button></a></li>
           {/* <li><a href='/' className="nav-link px-2 link-dark">Products</a></li> */}
         </ul>
-        <form className="col-12 col-lg-4 mb-3 mb-lg-0 me-lg-0 text-center"  role="search">
-          <input type="search" className="form-control border-success text-end" placeholder="Search Here..." aria-label="Search"/>
-        </form>
+       
 
         <div className={`nav-item nav-link form-check mx-2  form-switch text-${props.mode==='dark'?'light':'secondary'}`}>
                             {/* <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/> */}
