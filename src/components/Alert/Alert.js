def1c0860fdab86fbeeff02a00fb941037a0426a
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-11-22 10:21:40 +0600

    Update Alert.js

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -5,9 +5,11 @@ export default function Alert(props) {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
   return (
-    props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style={{height:'50px'}}>
+    {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capitalize(props.alert.type)}fully!</strong> {capitalize(props.alert.message)}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>}
     </div>
   )
 }
