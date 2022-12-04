182154b9f5984b16136badd24a20f9b6b00b0664
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-05 01:21:25 +0600

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -73,7 +73,7 @@ export default function Cart() {
         <form className="needs-validation" noValidate/>
           <div className="row g-3">
             <div className="col-sm-6">
-              <label for="firstName" className="form-label">First name</label>
+              <label htmlFor="firstName" className="form-label">First name</label>
               <input type="text" className="form-control" id="firstName" placeholder="" defaultValue="" required/>
               <div className="invalid-feedback">
                 Valid first name is required.
@@ -82,14 +82,14 @@ export default function Cart() {
 
             <div className="col-sm-6">
               <label htmlFor="lastName" className="form-label">Last name</label>
-              <input type="text" className="form-control" id="lastName" placeholder="" value="" required/>
+              <input type="text" className="form-control" id="lastName" placeholder="" defaultValue="" required/>
               <div className="invalid-feedback">
                 Valid last name is required.
               </div>
             </div>
 
             <div className="col-12">
-              <label for="username" className="form-label">Username</label>
+              <label htmlFor="username" className="form-label">Username</label>
               <div className="input-group has-validation">
                 <span className="input-group-text">@</span>
                 <input type="text" className="form-control" id="username" placeholder="Username" required/>
@@ -100,7 +100,7 @@ export default function Cart() {
             </div>
 
             <div className="col-12">
-              <label for="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
+              <label htmlFor="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
               <input type="email" className="form-control" id="email" placeholder="you@example.com"/>
               <div className="invalid-feedback">
                 Please enter a valid email address for shipping updates.
@@ -108,7 +108,7 @@ export default function Cart() {
             </div>
 
             <div className="col-12">
-              <label for="address" className="form-label">Address</label>
+              <label htmlFor="address" className="form-label">Address</label>
               <input type="text" className="form-control" id="address" placeholder="1234 Main St" required/>
               <div className="invalid-feedback">
                 Please enter your shipping address.
@@ -116,14 +116,14 @@ export default function Cart() {
             </div>
 
             <div className="col-12">
-              <label for="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
+              <label htmlFor="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
               <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"/>
             </div>
 
             <div className="col-md-5">
-              <label for="country" className="form-label">Country</label>
+              <label htmlFor="country" className="form-label">Country</label>
               <select className="form-select" id="country" required>
-                <option value="">Choose...</option>
+                <option defaultValue="">Choose...</option>
                 <option>United States</option>
               </select>
               <div className="invalid-feedback">
@@ -132,9 +132,9 @@ export default function Cart() {
             </div>
 
             <div className="col-md-4">
-              <label for="state" className="form-label">State</label>
+              <label htmlFor="state" className="form-label">State</label>
               <select className="form-select" id="state" required>
-                <option value="">Choose...</option>
+                <option defaultValue="">Choose...</option>
                 <option>California</option>
               </select>
               <div className="invalid-feedback">
@@ -143,7 +143,7 @@ export default function Cart() {
             </div>
 
             <div className="col-md-3">
-              <label for="zip" className="form-label">Zip</label>
+              <label htmlFor="zip" className="form-label">Zip</label>
               <input type="text" className="form-control" id="zip" placeholder="" required/>
               <div className="invalid-feedback">
                 Zip code required.
@@ -155,12 +155,12 @@ export default function Cart() {
 
           <div className="form-check">
             <input type="checkbox" className="form-check-input" id="same-address"/>
-            <label className="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
+            <label className="form-check-label" htmlFor="same-address">Shipping address is the same as my billing address</label>
           </div>
 
           <div className="form-check">
             <input type="checkbox" className="form-check-input" id="save-info"/>
-            <label className="form-check-label" for="save-info">Save this information for next time</label>
+            <label className="form-check-label" htmlFor="save-info">Save this information for next time</label>
           </div>
 
           <hr className="my-4"/>
@@ -170,21 +170,21 @@ export default function Cart() {
           <div className="my-3">
             <div className="form-check">
               <input id="credit" name="paymentMethod" type="radio" className="form-check-input" defaultChecked required/>
-              <label className="form-check-label" for="credit">Credit card</label>
+              <label className="form-check-label" htmlFor="credit">Credit card</label>
             </div>
             <div className="form-check">
               <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required/>
-              <label className="form-check-label" for="debit">Debit card</label>
+              <label className="form-check-label" htmlFor="debit">Debit card</label>
             </div>
             <div className="form-check">
               <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required/>
-              <label className="form-check-label" for="paypal">PayPal</label>
+              <label className="form-check-label" htmlFor="paypal">PayPal</label>
             </div>
           </div>
 
           <div className="row gy-3">
             <div className="col-md-6">
-              <label for="cc-name" className="form-label">Name on card</label>
+              <label htmlFor="cc-name" className="form-label">Name on card</label>
               <input type="text" className="form-control" id="cc-name" placeholder="" required/>
               <small className="text-muted">Full name as displayed on card</small>
               <div className="invalid-feedback">
@@ -193,7 +193,7 @@ export default function Cart() {
             </div>
 
             <div className="col-md-6">
-              <label for="cc-number" className="form-label">Credit card number</label>
+              <label htmlFor="cc-number" className="form-label">Credit card number</label>
               <input type="text" className="form-control" id="cc-number" placeholder="" required/>
               <div className="invalid-feedback">
                 Credit card number is required
@@ -201,7 +201,7 @@ export default function Cart() {
             </div>
 
             <div className="col-md-3">
-              <label for="cc-expiration" className="form-label">Expiration</label>
+              <label htmlFor="cc-expiration" className="form-label">Expiration</label>
               <input type="text" className="form-control" id="cc-expiration" placeholder="" required/>
               <div className="invalid-feedback">
                 Expiration date required
@@ -209,7 +209,7 @@ export default function Cart() {
             </div>
 
             <div className="col-md-3">
-              <label for="cc-cvv" className="form-label">CVV</label>
+              <label htmlFor="cc-cvv" className="form-label">CVV</label>
               <input type="text" className="form-control" id="cc-cvv" placeholder="" required/>
               <div className="invalid-feedback">
                 Security code required
