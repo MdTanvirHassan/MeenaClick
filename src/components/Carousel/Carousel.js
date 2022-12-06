27df6f6a40e18ceedc016d51f0d28ae276a76b80
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-07 01:17:19 +0600

    Update Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import slider from '../../image/s1.jpg';
 import slider2 from '../../image/s2.jpg';
 import slider3 from '../../image/s3.jpg';
@@ -65,44 +66,44 @@ export default function Carousel(props) {
       
       <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12 col-xl-3 col-xxl-3 ">
       <div className={`list-group bg-${props.mode==='dark'?'secondary':'light'} `}>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'danger':'danger'}  fw-bold`} >
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'danger':'danger'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'danger':'danger'} bi bi-bag-fill me-1`}></i>
-                    Essentials </a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'danger':'danger'}  fw-bold`} >
+                    Essentials </Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'danger':'danger'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'danger':'danger'} bi bi-box-fill me-1`}></i>
-                        Frozen Item</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Frozen Item</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-alexa me-1`}></i>
-                        Accessories</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Accessories</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-people-fill me-1`}>
-                            </i>Baby Care</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                            </i>Baby Care</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-columns-gap me-1`}></i>
-                        Bakery and Snacks</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Bakery and Snacks</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-amd me-1`}></i>
-                        Beauty and Hygiene</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Beauty and Hygiene</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-archive-fill me-1`}></i>
-                        Beverages</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Beverages</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-slack me-1`}></i>
-                        Fish</a>
-                    <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Fish</Link>
+                    <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-bag me-1`}></i>
-                        Fresh Product</a>
-                        <a href='/' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
+                        Fresh Product</Link>
+                        <Link href='/Combo' className={`list-group-item list-group-item-action bg-${props.mode==='dark'?'secondary':'light'} text-${props.mode==='dark'?'white':'dark'}  fw-bold`} >
                         <i className={`text-${props.mode==='dark'?'white':'success'} bi bi-house me-1`}></i>
-                        Housing & cleaning</a>
+                        Housing & cleaning</Link>
                     
                     
                     </div>
         
-      </div>
+            </div>
 
-    </div>
-  </div>           
+            </div>
+        </div>           
         </div>
     </div>
   )
