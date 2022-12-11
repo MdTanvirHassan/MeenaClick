bbf04791a6c7fccd18755fcf387d3a3349fa5849
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-11 14:45:57 +0600

    update offer products.

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -8,6 +8,12 @@ import img3 from '../../image/p6.jpg';
 import img4 from '../../image/p3.jpg';
 import img5 from '../../image/p4.jpg';
 import img6 from '../../image/p5.jpg';
+import org4 from "../../image/org4.jpg";
+import org5 from "../../image/org5.jpg";
+import org6 from "../../image/org6.jpg";
+import org7 from "../../image/org7.jpg";
+import org8 from "../../image/org8.jpg";
+import org9 from "../../image/org9.jpg";
 // import Footer from '../Footer/Footer';
 
 import { Link } from 'react-router-dom';
@@ -42,30 +48,75 @@ export default function Offer(props) {
           :
     <div className='mx-4'>
         <div className="text-success text-center">
-            <h4 className="">Offers</h4><hr />
+            <h4 className="">Offer Products</h4><hr />
         </div>
         <div className="row">
-          <div className="col-2">
+          {/* <div className="col-2">
 
           </div>
-          <div className="col-10">
+          <div className="col-10"> */}
+
+<div className="row my-2 mx-4">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org4} category="Grocery" title="Organikare Safe Ins Hand Sanitizer spray 100ml" offerPrice="140.00" price="130.00" />
+               
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org5} category="Grocery" title="Organikare Skin Lotion Dry Skin 200ml" offerPrice="190.00" price="175.00" />
+                
+                </div>
+                
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org6} category="Grocery" title="Organikare Glycerine 120ml" offerPrice="190.00" price="160.00" />
+                
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org7} category="Grocery" title="Organikare Glycerine 60ml" offerPrice="135.00" price="120.00" />
+                
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org8} category="Grocery" title="Organikare Hair Oil 75ml" offerPrice="1090.00" price="995.00" />
+                
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
+                
+                    <Grocery mode={props.mode} notify={props.notify} 
+                    img={org9} category="Grocery" title="Organikare Face Wash Cleansing Milk 100gm" offerPrice="360.00" price="328.00" />
+                
+                </div>
+                       
+            </div>
 
           
         
         <div className={`row mx-lg- mx-xxl-3 mx-xl-3 mx-md-3 my-1 mb-1 text-${props.mode==='dark'?'white':'body'} `}>
-                <div className="col-lg-3 col-md-4 col-6 col-xl-3 col-xxl-2 col-xs-6">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
                 <Grocery mode={props.mode} notify={props.notify} img={img} category="Grocery" title="Radhuni MAster Oil 1Ltr" offerPrice="tk. 90.00" price="80.00"></Grocery>
                 </div>
-                <div className="col-lg-3 col-md-4 col-6 col-xl-3 col-xxl-2 col-xs-6">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
                 <Grocery mode={props.mode} notify={props.notify} img={img4} category="Grocery" title="Span Virgin Olive Oil 1ltr" offerPrice="tk. 165.00" price="145.00"></Grocery>
                 </div>
-                <div className="col-lg-3 col-md-4 col-6 col-xl-3 col-xxl-2 col-xs-6">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
                 <Grocery mode={props.mode} notify={props.notify} img={img3} category="Grocery" title="Aarong Butter  200gm" offerPrice="tk. 195.00" price="160.00"></Grocery>
                 </div>
-                <div className="col-lg-3 col-md-4 col-6 col-xl-3 col-xxl-2 col-xs-6">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
                 <Grocery mode={props.mode} notify={props.notify} img={img2} category="Grocery" title="Aarong Ghee  300gm" offerPrice="tk. 145.00" price="120.00"></Grocery>
                 </div>
-                <div className="col-lg-3 col-md-4 col-6 col-xl-3 col-xxl-2 col-xs-6">
+                <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
                 <Grocery mode={props.mode} notify={props.notify} img={img5} category="Grocery" title="Olitalia Sunflower Oil 5ltr" offerPrice="tk. 1555.00" price="1505.00"></Grocery>
                 </div>
                 <div className="col-lg-2 col-md-4 col-6 col-xl-2 col-xxl-2 col-xs-6">
@@ -95,7 +146,7 @@ export default function Offer(props) {
               </nav>
               </div>  
               </div>   
-    </div>
+    // </div>
     
     }
     </>
