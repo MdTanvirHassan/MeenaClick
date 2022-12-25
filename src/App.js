c8add30dd2063320aef0a2d63a3d79149feb2e29
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-26 02:03:32 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,6 +100,7 @@ function App() {
             <LoadingBar
                 color='#f11946'
                 progress={progress}
+                height={3}
                 onLoaderFinished={() => setProgress(0)}
             />
           <Alert alert={alert} />
